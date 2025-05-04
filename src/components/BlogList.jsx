bfc9a43a470ff26c222d0ab7c440576b492403a7
Louis2324 <miguellouis2023@gmail.com>
2025-05-04 12:25:14 +0200

    Added props functionalities

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const BlogList = (props) => {
+  const blogs = props.blogs;
+  const title = props.title;
+  return (
+    <div className="blog-list">
+      <h2 className="blog-title">{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.content}</p>
+          <small>Written by {blog.author}</small>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
