cd5ca7f8271542f3459b68ef5d2e2b2e9cccb315
Louis2324 <miguellouis2023@gmail.com>
2025-05-04 12:26:18 +0200

    Changed the files to make use of the blog list component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,30 @@
-import React from "react";
-import { useState } from "react";
-
+import React, { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
-  //   let name = "Mario";
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    setName("Luigi");
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new Website",
+      content: "Lorem ipsum ...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome Party",
+      content: "Lorem ipsum ...",
+      author: "luigi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      content: "Lorem ipsum ...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-  return (
-    <div className="home">
-      <h2>Home Page</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick}>Click Me</button>
-    </div>
-  );
+  return <div className="home">
+        <BlogList blogs = {blogs}  title ="All Blogs"/>
+  </div>;
 };
 
 export default Home;
