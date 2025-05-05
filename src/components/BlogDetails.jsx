50f19553f2edca4dbee71373964dcc34a4f2a1ca
Louis2324 <miguellouis2023@gmail.com>
2025-05-05 15:50:40 +0200

    Added a blog detail functionality to read a blog individually from the home page

---
+++ src/components/BlogDetails.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch(`http://localhost:3000/blogs/${id}`);
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <div>{blog.content}</div>
+          <small>Written by {blog.author}</small>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
