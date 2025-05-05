3d524c3b38a9cf756b51784f0e6f5c45716bba56
Louis2324 <miguellouis2023@gmail.com>
2025-05-05 10:38:57 +0200

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Navbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Create from "./components/Create.jsx";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
