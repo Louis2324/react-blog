6152b3090672913d2834b5a49643d6f08998fd0a
Louis2324 <miguellouis2023@gmail.com>
2025-05-05 09:27:49 +0200

    Added some simple error handling for when fetching data to our virtual db

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,33 @@ import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3000/blogs/")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Couldn't fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {error && <div> {error} </div>}
+      {isPending && <div className="loading"> Loading...</div>}
+      {!isPending && blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
