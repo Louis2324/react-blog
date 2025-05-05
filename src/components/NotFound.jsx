9b4d15bf50df77707c8da5ae007c0610cc5f1d8c
Louis2324 <miguellouis2023@gmail.com>
2025-05-05 17:31:21 +0200

    Added a not found page to intercept bad requests from users

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h2>Not Found</h2>
+      <p>That specified page can not be found</p>
+      <Link to="/"> Go back Home 😅😅 ...?</Link>
+    </div>
+  );
+};
+
+export default NotFound;
