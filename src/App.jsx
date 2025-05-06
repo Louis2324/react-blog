bf3106bc4f8a1aa193c7e5940ae347acfb6597ed
Louis2324 <miguellouis2023@gmail.com>
2025-05-06 07:23:18 +0200

    Added theme toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./components/Create.jsx";
 import BlogDetails from "./components/BlogDetails.jsx";
 import NotFound from "./components/NotFound.jsx";
-function App() {
+
+const toggleTheme = () => {
+  const currentTheme = document.body.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    document.body.setAttribute("data-theme", "light");
+  } else {
+    document.body.setAttribute("data-theme", "dark");
+  }
+};
+
+function App() { 
   return (
     <Router>
       <div className="App">
@@ -14,9 +24,10 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/create" element={<Create />} />
             <Route exact path="/blogs/:id" element={<BlogDetails />} />
-            <Route path="*" element={ <NotFound /> }/>
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
+        <button className="theme-toggle" onClick={() => {toggleTheme()}}></button>
       </div>
     </Router>
   );
