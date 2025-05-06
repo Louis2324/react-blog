b657126fe69ef6689b2217ea1e391604a8dc8ac5
Louis2324 <miguellouis2023@gmail.com>
2025-05-06 07:21:51 +0200

    Added better linking with the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
-        <h1>The Dojo Blog</h1>
+        <Link to="/"><h1>The Dojo Blog</h1></Link>
         <div className="links">
           <Link to="/">Home</Link>
           <Link to="/create">New Blog</Link>
