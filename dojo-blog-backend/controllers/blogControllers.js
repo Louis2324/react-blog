b9e9abda565d3846ca81d5490081f4c51829ac97
Louis2324 <miguellouis2023@gmail.com>
2025-06-05 08:09:33 +0200

    Added proper logic such that a write can delete their own posts

--- dojo-blog-backend/controllers/blogControllers.js
+++ dojo-blog-backend/controllers/blogControllers.js
@@ -12,14 +12,17 @@ export const getBlog = async (req, res) => {
 };
 
 export const createBlog = async (req, res) => {
-  const blog = new Blog(req.body);
-  await blog.save();
-  res.status(201).json(blog);
+  try {
+    const blog = await Blog.create({ ...req.body, author: req.user.id });
+    res.status(201).json(blog);
+  } catch (error) {
+    return res.status(500).json({ msg: "Error creating post: " + error });
+  }
 };
 
 export const updateBlog = async (req, res) => {
   const blog = await Blog.findById(req.params.id);
-  if(!blog) return res.status(404).json({msg:"Not Found"});
+  if (!blog) return res.status(404).json({ msg: "Not Found" });
   blog.title = req.body.title || blog.title;
   blog.body = req.body.body || blog.body;
   await blog.save();
@@ -27,7 +30,20 @@ export const updateBlog = async (req, res) => {
 };
 
 export const deleteBlog = async (req, res) => {
-  const blog = await Blog.findByIdAndDelete(req.params.id);
-  if (!blog) return res.status(404).json({ msg: "Blog Not Found" });
-  res.status(200).json({ message: "Blog deleted" });
+  const { id } = req.params;
+  try {
+    const blog = await Blog.findById(id);
+    if (!blog) return res.status(400).json({ msg: "Not found" });
+    if (blog.author.toString() !== req.user.id && req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ msg: "Unauthorized can't delete this post" });
+    }
+    await blog.deleteOne();
+    return res.status(200).json({ msg: "Blog Deleted Succesfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "There was a problem deleting this post: " + error });
+  }
 };
